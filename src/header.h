4483b21b6e677cbd919f6a3d0669e889c2ff31d9
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-09 02:11:25 -0400

    add bools and print table prototype
    the bool is needed for the awsum_mode

--- src/header.h
+++ src/header.h
@@ -4,14 +4,19 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define NUM_WORDS 2327
 #define FILTER_ARG_EXPECTED 3
 #define ARGS_BEFORE_FLAG 1
 
 extern char words[NUM_WORDS][6];
+extern char filtered_arr[NUM_WORDS][6];
 
 void test(int num_arg);
 void parse_command(int num_arg, char *argument[]);
 void debug(int num_arg, char *argument[]);
 void err(int error_code);
+
+void print_as_table(int width, int total_elements, bool awsum_mode);
+
