b9e936e2de96c7d94949f9df7a1e84f6dfd023bb
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-08 20:27:05 -0400

    add stuff

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    test(argc);
+    parse_command(argc, argv);
     return 0;
 }
 
@@ -16,3 +16,30 @@ void test(int num_arg)
 
     printf("\n");
 }
+
+void parse_command(int num_arg, char *argument[])
+{
+    if (num_arg < 2)
+    {
+	printf("You haven't used any argments in your command");
+	exit(1);
+    }
+    for (int i = 0; i < num_arg; i++)
+    {
+	printf("%s\n", argument[i]);
+    }
+
+    // set the amount of filtered words but inclusively
+    int inc_filtered_num = NUM_WORDS;
+
+    for (int i = 0; i < NUM_WORDS; i++)
+    {
+	if ('Z' == words[i][0])
+	{
+	    printf("True for %d\n", i);
+	}
+    }
+
+    //for (int i = 0; i < 
+}
+
