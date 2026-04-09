dbc544ea4e4063b4eda5c298a0a50078d1fd3f2f
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-08 23:56:01 -0400

    add complete header file

--- src/header.h
+++ src/header.h
@@ -2,3 +2,16 @@
 #define _POSIX_C_SOURCE 200809L
 
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_WORDS 2327
+#define FILTER_ARG_EXPECTED 3
+#define ARGS_BEFORE_FLAG 1
+
+extern char words[NUM_WORDS][6];
+
+void test(int num_arg);
+void parse_command(int num_arg, char *argument[]);
+void debug(int num_arg, char *argument[]);
+void err(int error_code);
