4785d787fda9acbf04db2ba100b4a3b323f09fac
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-10 23:09:29 -0400

    add table width variable

--- src/config.c
+++ src/config.c
@@ -6,3 +6,6 @@
 bool awsum_table_mode = false; // false by default
 // if true, print items in columns and then go to the next column
 // if false, print items from left to right and then go to next row(down)
+
+int table_width = 6; // 6 by default
+// this is the width of the table (the number of words displayed on the x axis)
