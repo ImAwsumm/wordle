fab6e8a6447b18884a2642c8971c33aa3bcc2acf
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-10 22:43:39 -0400

    add comments for clarity

--- src/config.c
+++ src/config.c
@@ -1,3 +1,8 @@
 #include "header.h"
 
-bool awsum_table_mode = false;
+
+// display options
+
+bool awsum_table_mode = false; // false by default
+// if true, print items in columns and then go to the next column
+// if false, print items from left to right and then go to next row(down)
