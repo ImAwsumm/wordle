e6e5bfaa863fd6e07c0a2b05938195860aca251c
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-11 14:54:03 -0400

    add mandatory flag indexing global variables

--- src/header.h
+++ src/header.h
@@ -9,18 +9,23 @@
 #define NUM_WORDS 2331
 #define NUM_LETTERS_WORD 5
 
-#define ARGS_BEFORE_FLAG 1
+#define ARGS_BEFORE_FLAG_BASE 1
 #define P_FILTERS_ARG_EXP 3
+#define G_FILTERS_ARG_EXP 2
 
 extern char words[NUM_WORDS][6];
 extern char filtered_arr[NUM_WORDS][6];
 
 void test(int num_arg);
 void parse_command(int num_arg, char *argument[]);
-void debug(int num_arg, char *argument[]);
 void err(int error_code);
 
 void print_as_table(int width, int total_elements, bool awsum_mode);
+
+// Global variables
+extern int ARGS_BEFORE_CUR_FLAG;
+extern int flag_r;
+
 
 // General simple functions
 void user_index_validation(int index);
@@ -29,3 +34,4 @@ void user_index_validation(int index);
 // config file
 extern bool awsum_table_mode;
 extern int table_width;
+
