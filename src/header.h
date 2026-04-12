dbb5030620dfd68683de494ad51465ac5eefbda9
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-12 16:50:13 -0400

    add colours & use INDEX_LETTERS_WORD

--- src/header.h
+++ src/header.h
@@ -8,6 +8,8 @@
 
 #define NUM_WORDS 2331
 #define NUM_LETTERS_WORD 5
+// INDEX_LETTERS_WORD is NUM_LETTERS + 1
+#define INDEX_LETTERS_WORD 6
 
 #define ARGS_BEFORE_FLAG_BASE 1
 #define P_FILTERS_ARG_EXP 3
@@ -18,6 +20,10 @@
 #define ANSI_GREEN 	"\x1b[32m" 
 #define ANSI_CYAN    	"\x1b[36m"
 #define ANSI_LCYAN    	"\x1b[96m"
+#define ANSI_YELLOW 	"\x1b[33m"
+#define ANSI_LYELLOW 	"\x1b[93m"
+#define ANSI_BLUE    	"\x1b[34m"
+#define ANSI_WHITE    	"\x1b[97m"
 
 #define BOLD_S  	"\x1B[1m" 
 
@@ -25,8 +31,9 @@
 #define STYLE_END	"\x1B[0m" 
 
 
-extern char words[NUM_WORDS][6];
-extern char filtered_arr[NUM_WORDS][6];
+extern char words[NUM_WORDS][INDEX_LETTERS_WORD];
+extern char filtered_arr[NUM_WORDS][INDEX_LETTERS_WORD];
+
 
 void test(int num_arg);
 void parse_command(int num_arg, char *argument[]);
@@ -49,3 +56,4 @@ extern int table_width;
 extern bool verbose;
 
 void verbose_printing(char *flag, char letter, int indexed_letter_value, int affected_words, bool letter_is_present);
+
