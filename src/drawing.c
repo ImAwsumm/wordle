b49d9d9b22c06b84380fc57a782c1a85b856c2c4
imawsumm <imawsumm@users.noreply.github.com>
2026-05-09 23:55:07 -0400

    feat: add word parsing in drawing.c
    also add num_answers

--- src/drawing.c
+++ src/drawing.c
@@ -12,17 +12,21 @@ void drawing(char wordle_answer[INDEX_LETTERS_WORD])
 	bool x_pattern false;
 
 	char all_answers[5][ALLWORDS];
+
 	if (!x_pattern)
 	{
 		for (int i = 0; i < 5; i++)
 		{
-			char filtered_temp_arr[NUM_ALL_WORDS];
-			for (int j = 0; i < NUM_ALL_WORDS; i++)
+			char filtered_words[NUM_ALL_WORDS];
+			int num_answers = 0;
+			for (int j = 0; j < NUM_ALL_WORDS; j++)
 			{
-				if ()
+				if (wordle_answer[i] == all_words[j][i])
 				{
+					strcpy(filtered_words[j], all_words[j]);
 				}
 			}
+			/* printf("%s\n"); */
 		}
 	}
 	
