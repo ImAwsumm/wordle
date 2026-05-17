6616a9a0191c7f227e27556a2c4f744e315261f7
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-05-17 00:11:10 -0400

    build: remove useless code
    also make use of the new command we calculated

--- build.c
+++ build.c
@@ -15,18 +15,6 @@ char *GCC_COMPILER_NAME = "gcc";
 #define fp_size 32
 #define base_args 1
 
-char PARSING_FILE_PATH[fp_size] = "src/parsing";
-char CONFIG_FILE_PATH[fp_size] = "src/config";
-char MAIN_FILE_PATH[fp_size] = "src/main";
-char CMDPARS_FILE_PATH[fp_size] = "src/command-parsing";
-char DRAWING_FILE_PATH[fp_size] = "src/drawing";
-char FUNCTIONS_FILE_PATH[fp_size] = "src/functions";
-char ERRORS_FN_FILE_PATH[fp_size] = "src/errors";
-
-char ALL_WORDS_FILE_PATH[fp_size] = "src/word-lists/all-words";
-char COM_WORDS_FILE_PATH[fp_size] = "src/word-lists/common-words";
-char NYT_WORDS_FILE_PATH[fp_size] = "src/word-lists/words";
-
 char *all_src_files[num_src_files] = 
 {
 	"src/parsing",
@@ -56,7 +44,6 @@ compiler_enum compiler_choice;
 
 int main (int argc, char *argv[])
 {
-	int total_flags_size = 16;
 	bool Wall_flag = false;
 	bool Wextra_flag = false;
 	bool Wconversion_flag = false;
@@ -65,8 +52,6 @@ int main (int argc, char *argv[])
 	bool C99_flag = false;
 	bool Wshadow_flag = false;
 	bool verbose = true; /* false by default */
-
-	int flag_mem_size = 16;
 
 	int num_flags = 0;
 	if (argc > base_args)
@@ -153,56 +138,9 @@ int main (int argc, char *argv[])
 				exit(1);
 			}
 		}
-
-		if (num_flags > 0)
-		{
-			int temp_total_flag_size = flag_mem_size * num_flags;
-			total_flags_size += temp_total_flag_size;
-		}
 	}
-
-	const char *src_files_template = " %s %s %s %s.c %s.c %s.c %s.c %s.c %s.c %s.c ";
-	char SRC_ALL_WORDS[fp_size];
-	char SRC_COM_WORDS[fp_size];
-	char SRC_NYT_WORDS[fp_size];
-	
-	char OBJ_ALL_WORDS_FP[fp_size];
-	char OBJ_COM_WORDS_FP[fp_size];
-	char OBJ_NYT_WORDS_FP[fp_size];
-
-	strcpy(SRC_ALL_WORDS, ALL_WORDS_FILE_PATH);
-	strcpy(SRC_COM_WORDS, COM_WORDS_FILE_PATH);
-	strcpy(SRC_NYT_WORDS, NYT_WORDS_FILE_PATH);
-
-	strcat(SRC_ALL_WORDS, src_file_extention);
-	strcat(SRC_COM_WORDS, src_file_extention);
-	strcat(SRC_NYT_WORDS, src_file_extention);
-
-	if (compile_into_objects)
-	{
-		/* create and initialize object file paths */
-		strcpy(OBJ_ALL_WORDS_FP, ALL_WORDS_FILE_PATH);
-		strcpy(OBJ_COM_WORDS_FP, COM_WORDS_FILE_PATH);
-		strcpy(OBJ_NYT_WORDS_FP, NYT_WORDS_FILE_PATH);
-
-		strcat(OBJ_ALL_WORDS_FP, obj_file_extention);
-		strcat(OBJ_COM_WORDS_FP, obj_file_extention);
-		strcat(OBJ_NYT_WORDS_FP, obj_file_extention);
-	}
-
-	int mem_needed_src = 0;
 	if (!compile_into_objects)
 	{
-		char *parsing = PARSING_FILE_PATH;
-		char *configp = CONFIG_FILE_PATH;
-		char *main_fp = MAIN_FILE_PATH;
-		char *cmd_parsing = CMDPARS_FILE_PATH;
-		char *drawingpath = DRAWING_FILE_PATH;
-		char *func_path = FUNCTIONS_FILE_PATH;
-		char *errs_path = ERRORS_FN_FILE_PATH;
-
-		mem_needed_src = 1 + snprintf(NULL, 0, src_files_template, SRC_NYT_WORDS, SRC_ALL_WORDS, SRC_COM_WORDS, errs_path, func_path, cmd_parsing, configp, parsing, drawingpath, main_fp);
-
 		char compiler[compiler_name_len];
 
 		switch (compiler_choice)
@@ -224,14 +162,10 @@ int main (int argc, char *argv[])
 				exit(1);
 		}
 
-		int mem_needed = mem_needed_src + snprintf(NULL, 0, "%s ", compiler);
-
-
 		char *flags_string_base = "-o wordle";
 
-		int mem_needed_all_c_flags = 1;
-
 		char all_custom_flags[1024] = ""; 
+
 		if (num_flags > 0)
 		{
 			if (Wall_flag)
@@ -262,34 +196,14 @@ int main (int argc, char *argv[])
 			{
 				strcat(all_custom_flags, "-std=c99 ");
 			}
-
-			mem_needed_all_c_flags = 1 + snprintf(NULL, 0, "%s ", all_custom_flags);
 		}
 
 		int mem_needed_output_flags = snprintf(NULL, 0, "%s", flags_string_base);
 		mem_needed_output_flags++;
-		total_flags_size += mem_needed_output_flags;
-		mem_needed += mem_needed_all_c_flags;
-		mem_needed += total_flags_size;
 		char flags_string[mem_needed_output_flags];
 		snprintf(flags_string, (size_t)mem_needed_output_flags, "%s", flags_string_base);
 
 		/* TODO craft cmd */
-
-		char source_files[mem_needed_src];
-		snprintf(source_files, (size_t)mem_needed_src, src_files_template, SRC_NYT_WORDS, SRC_ALL_WORDS, SRC_COM_WORDS, cmd_parsing, main_fp, errs_path, func_path, drawingpath, configp, parsing);
-
-		char safe_cmd[mem_needed];
-
-		snprintf(safe_cmd, (size_t)mem_needed, "%s%s%s %s", compiler, source_files, flags_string, all_custom_flags);
-
-		if (verbose)
-		{
-			printf("%s\n", safe_cmd);
-		}
-
-		system(safe_cmd);
-
 		int full_cmp_size = 1; /* for the NULL terminator */
 		int cmd_cmp_size[num_src_files];
 
@@ -318,7 +232,11 @@ int main (int argc, char *argv[])
 		strcat(full_compilation_path, " ");
 		strcat(full_compilation_path, all_custom_flags);
 
-		printf("%s\n", full_compilation_path);
+		if (verbose)
+		{
+			printf("%s\n", full_compilation_path);
+		}
+		system(full_compilation_path);
 	}
 	return 0;
 }
