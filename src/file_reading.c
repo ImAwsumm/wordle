904e9243a924501e5d398e2970d87803c95e7ce4
emile-ross <emileross@tutanota.de>
2026-06-30 22:41:09 -0400

    fix: invalid memory allocation
    this was allocating memory for 6 bytes instead of 7
    we need 7 because the 5 letter word has a newline (\n) and a null
    terminator (\0)

--- src/file_reading.c
+++ src/file_reading.c
@@ -13,43 +13,55 @@ uint16_t get_num_lines(char **file_path)
 	{
 		err(NO_SUCH_FILE);
 	}
-	
+
 	char char_read = '\0';
 	for (char_read = (char)fgetc(word_file); char_read != EOF; char_read = (char)fgetc(word_file))
 	{
-		if (char_read == '\n')
+		if (char_read == '\n')    
 		{
 			line_count++;
 		}
 	}
-	
+
+	/* count the last line even though it might end in EOF instead of \n */
+	if (line_count > 0 || fgetc(word_file) != EOF)
+	{
+		line_count++;
+	}
+
 	fclose(word_file);	/* close file */
 
 	return line_count;
 }
 
-char (*read_words(char **file_path, uint16_t *num_words))[INDEX_LETTERS_WORD]
+char (*read_words(char **file_path, uint16_t *num_words))[7]
 {
 
 	/* allocate memory for all words
 	 * multiply the size of 1 word by the number of words to get the total size */
-	size_t word_list_size = (*num_words * sizeof(char[INDEX_LETTERS_WORD]));
-	char (*ptr)[INDEX_LETTERS_WORD] = malloc(word_list_size);
+	size_t word_list_size = (*num_words * sizeof(char[7]));
+	char (*ptr)[7] = malloc(word_list_size);
 
 	FILE *file = fopen(*file_path, "r");
 	
 	for (uint16_t i = 0; i < *num_words; i++) 
 	{
-		if (fgets(ptr[i], INDEX_LETTERS_WORD, file) == NULL) 
+		if (fgets(ptr[i], 7, file) == NULL) 
 		{
 			break;
 		}
 
+		/* replace last character (before NULL terminator) when it's a newline */
 		size_t len = strlen(ptr[i]);
 		if (len > 0 && ptr[i][len - 1] == '\n') 
 		{
 			ptr[i][len - 1] = '\0';
 		}
+		if (len > 0 && ptr[i][len - 1] == '\r')
+		{
+			ptr[i][len - 1] = '\0';
+		}
+
 	}
 	
 	fclose(file);	/* close file after use */
@@ -57,3 +69,4 @@ char (*read_words(char **file_path, uint16_t *num_words))[INDEX_LETTERS_WORD]
 	/* the memory needs to be freed later after use */
 	return ptr;
 }
+
