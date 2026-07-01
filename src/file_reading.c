ebd56c0b679bc15dbf585510ce32142d83b7257c
emile-ross <emileross@tutanota.de>
2026-06-30 21:28:28 -0400

    feat: add file reading and read_words() function
    this function takes the number of words and allocates memory based on
    the number of words
    this function then reads the file and writes all words read into the ptr
    buffer we've created
    the function then returns a pointer to that buffer

--- src/file_reading.c
+++ src/file_reading.c
@@ -1,10 +1,10 @@
 #include "header.h"
 
 /* there's no way there is more than 65535 words (can be changed to 32bit */
-uint16_t get_num_lines(char *file_path)
+uint16_t get_num_lines(char **file_path)
 {
 	/* open file path in reading mode */
-	FILE *word_file = fopen(file_path, "r");
+	FILE *word_file = fopen(*file_path, "r");
 
 	uint16_t line_count = 0; /* max size of 65535 (65535 lines) */
 
@@ -28,4 +28,32 @@ uint16_t get_num_lines(char *file_path)
 	return line_count;
 }
 
+char (*read_words(char **file_path, uint16_t *num_words))[INDEX_LETTERS_WORD]
+{
 
+	/* allocate memory for all words
+	 * multiply the size of 1 word by the number of words to get the total size */
+	size_t word_list_size = (*num_words * sizeof(char[INDEX_LETTERS_WORD]));
+	char (*ptr)[INDEX_LETTERS_WORD] = malloc(word_list_size);
+
+	FILE *file = fopen(*file_path, "r");
+	
+	for (uint16_t i = 0; i < *num_words; i++) 
+	{
+		if (fgets(ptr[i], INDEX_LETTERS_WORD, file) == NULL) 
+		{
+			break;
+		}
+
+		size_t len = strlen(ptr[i]);
+		if (len > 0 && ptr[i][len - 1] == '\n') 
+		{
+			ptr[i][len - 1] = '\0';
+		}
+	}
+	
+	fclose(file);	/* close file after use */
+
+	/* the memory needs to be freed later after use */
+	return ptr;
+}
