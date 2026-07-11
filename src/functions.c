a7ae772a32419700d7441fcf1fd6318f36b17c25
emile-ross <emileross@tutanota.de>
2026-07-10 22:26:25 -0400

    feat: free memory on error in word_list_name()
    fixes the memory leak in word_list_name() function

--- src/functions.c
+++ src/functions.c
@@ -15,22 +15,23 @@ uint8_t valid_user_index(long index, void *ptr_to_free)
 		/* display error message for invalid value (invalid index into the word) */
 		free(ptr_to_free);
 	    	err(CMD_INDEX_BOUNDS);
+		exit(1);
 	}
 	else if (index <= 0)
 	{
 		free(ptr_to_free);
 		err(INVALID_INDEX);
+		exit(1);
 	}
 	else
 	{
 		return (uint8_t)index;
 	}
-	return 255;
 }
 
 /* BTW: functions related to buffers were moved to src/buffers.c */
 
-char *word_list_name(ALL_WORD_LISTS word_list_type)
+char *word_list_name(ALL_WORD_LISTS word_list_type, void *buf)
 {
 	switch (word_list_type)
 	{
@@ -49,8 +50,9 @@ char *word_list_name(ALL_WORD_LISTS word_list_type)
 	case custom_list:
 		return "custom word list";
 	default:
+		free(buf);
 		err(UNKNOWN_WORD_LIST);
-		return NULL;
+		exit(1);
 	}
 }
 
--- src/header.h
+++ src/header.h
@@ -50,7 +50,7 @@
 
 /* Word lists */
 	char (*list_match(ALL_WORD_LISTS word_list_enum, uint32_t *number_of_words, bool standard_word_list))[6];
-	char *word_list_name(ALL_WORD_LISTS word_list_type);	/* returns the name of the word list */
+	char *word_list_name(ALL_WORD_LISTS word_list_type, void *buf);	/* returns the name of the word list */
 	extern uint8_t NUM_WORD_LISTS;
 
 /* filtered array used to store filtered words and it is also used for printing results */
