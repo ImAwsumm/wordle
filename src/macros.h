5b6a3c039c4e336ae83153a7f2eb66e679cf12d5
emile-ross <emileross@tutanota.de>
2026-07-28 22:15:15 -0400

    feat: add macros.h header file

---
+++ src/macros.h
@@ -0,0 +1,39 @@
+/* number of words for each array */
+	#define NUM_WORDS 2337
+	#define NUM_COMMON_WORDS 5700
+	#define NUM_ALL_WORDS 14862
+	
+	#define NUM_FR_ALL_WORDS  8065
+	
+	#define NUM_LA_ALL_WORDS 15842
+	#define NUM_LA_COM_WORDS 6662
+
+#define max_filename_length (128)
+
+#define NUM_LETTERS_WORD (5)
+#define INDEX_LETTERS_WORD (6)
+
+#define ARGS_BEFORE_FLAG_BASE (1)
+#define P_FILTERS_ARG_EXP (3)
+#define G_FILTERS_ARG_EXP (2)
+#define WORD_LIST_ARG_EXP (2)
+
+/* Define colours */
+#define ANSI_BLUE	"\x1b[34m"
+#define ANSI_CYAN	"\x1b[36m"
+#define ANSI_GREEN	"\x1b[32m" 
+#define ANSI_GREY	"\x1b[90m"
+#define ANSI_LCYAN	"\x1b[96m"
+#define ANSI_LYELLOW	"\x1b[93m"
+#define ANSI_RED	"\x1b[31m"
+#define ANSI_WHITE	"\x1b[97m"
+#define ANSI_YELLOW	"\x1b[33m"
+
+#define BOLD_S		"\x1B[1m" 
+#define UDRL_S  	"\x1B[4m" 
+
+/* reset styling */
+#define STYLE_END	"\x1B[0m" 
+
+#define max_valid_args (48)
+
