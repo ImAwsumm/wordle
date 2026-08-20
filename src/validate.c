2ecac6ac03354f4e0bd41f436ce0dbc25472a9ce
emile-ross <emileross@tutanota.de>
2026-08-19 23:02:57 -0400

    feat: add error checking in middle() fn
    checks if the integer has valid bounds

--- src/validate.c
+++ src/validate.c
@@ -73,5 +73,14 @@ void validate_word(char *command_word_string)
 int middle(int lb, int ub)
 {
 	int dif = ub - lb;
+	if (dif == 0)
+	{
+		return 0;
+	}
+	else if (dif < 0)
+	{
+		fprintf(stderr, "Invalid bounds, lower bound \"%d\" lb is greater than upper bound \"%d\"\n", lb, ub);
+		exit(1);
+	}
 	return (dif / 2);
 }
