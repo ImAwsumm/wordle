f6b2ffe2225b1040e6cabc8204dcd4635d1211d6
emile-ross <emileross@tutanota.de>
2026-08-19 22:39:18 -0400

    feat: add middle() & return value parsing
    gets the return value from the middle word comparaison

--- src/validate.c
+++ src/validate.c
@@ -17,6 +17,10 @@ void validate_word(char *command_word_string)
 			uint32_t num_words = 0;
 			char (*ptr)[INDEX_LETTERS_WORD] = list_match((ALL_WORD_LISTS)i, &num_words, true);
 	
+			int mid = middle(0, num_words - 1);
+			int ret = strcmp(command_word_string, ptr[mid]);
+
+
 			for (uint32_t j = 0; j < num_words; j++)
 			{
 				if (strcmp(command_word_string, ptr[j]) == 0)
@@ -65,6 +69,7 @@ void validate_word(char *command_word_string)
 	}
 }
 
+/* TODO take in int64_t and add error checking for possibly invalid values */
 int middle(int lb, int ub)
 {
 	int dif = ub - lb;
